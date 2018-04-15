09aa91b97afe668cb3206dcb52bc3213180b7436
huangqingshi <huangqingshi@jd.com>
2018-04-15 13:36:36 +0800

    修复一些文字

--- src/main/java/com/hqs/springboot/mapper/TestMapper.java
+++ src/main/java/com/hqs/springboot/mapper/TestMapper.java
@@ -10,7 +10,7 @@ import java.util.List;
  * @Author huangqingshi
  * @Date 2018.04.11
  * 接口，对Test表进行操作
- * 可以不用@Mapper，通过将如下代码加入到VideoDataApiApplication上面就行。
+ * 可以不用@Mapper，通过将如下代码加入到SpringBootDemoApiApplication上面就行。
  */
 @Mapper
 public interface TestMapper {
